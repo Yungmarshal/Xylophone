e2302d975162f31d255302f89788ae5687aee5c8
Yungmarshal <84972049+Yungmarshal@users.noreply.github.com>
2022-01-22 09:47:41 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(Xylophone());
+}
+
+class Xylophone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: FlatButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note1.wav');
+            },
+            child: Text('Play'),
+          ),
+        ),
+      ),
+    );
+  }
+}
